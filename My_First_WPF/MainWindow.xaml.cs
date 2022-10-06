c02a2a7139ad36857ddde68171b9541343002bbb
ninjafiveo <42717162+ninjafiveo@users.noreply.github.com>
2022-10-06 08:52:53 -0400

    Add project files.

---
+++ My_First_WPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace My_First_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void the_button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You Clicked Me");
+        }
+
+        private void the_button_LostFocus(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("HEY LOOK AT ME. HI MR. SEKOL");
+        }
+
+        private void the_button_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            MessageBox.Show("Don't Resize Me Man");
+        }
+
+        private void window_SizeChange(object sender, SizeChangedEventArgs e)
+        {
+            MessageBox.Show("Don't Resize Me Man 22222222222");
+        }
+
+        private void Another_Button_Mouse_Enter(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("STOP HOVERING OVER ME MAN");
+        }
+    }
+}
